b4907b9209f314d4347cc344f19a4e9c17fb8a55
Alina Komar <alinakomar11@gmail.com>
2026-04-17 19:57:54 +0300

    Add PipelineStepExecutionRepository

---
+++ 2026_MT_Komar_A_A_Lab_2/Repositories/IPipelineStepExecutionRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Repositories;
+
+using Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+#nullable enable
+public interface IPipelineStepExecutionRepository : IRepository<PipelineStepExecution>
+{
+    Task<IEnumerable<PipelineStepExecution>> GetByProjectIdAsync(int projectId);
+
+    Task<IEnumerable<PipelineStepExecution>> GetByStageTypeIdAsync(int stageTypeId);
+
+    Task<PipelineStepExecution?> GetWithLogsAsync(int id);
+}
+#nullable restore
---
+++ 2026_MT_Komar_A_A_Lab_2/Repositories/PipelineStepExecutionRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace Repositories;
+
+using Data;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class PipelineStepExecutionRepository(ApplicationDbContext context)
+    : Repository<PipelineStepExecution>(context), IPipelineStepExecutionRepository
+{
+    public async Task<IEnumerable<PipelineStepExecution>> GetByProjectIdAsync(int projectId)
+    {
+        return await this.DbSet
+            .Where(pse => pse.ProjectId == projectId)
+            .OrderByDescending(pse => pse.StartedAt)
+            .ToListAsync().ConfigureAwait(false);
+    }
+
+    public async Task<IEnumerable<PipelineStepExecution>> GetByStageTypeIdAsync(int stageTypeId)
+    {
+        return await this.DbSet
+            .Where(pse => pse.StageTypeId == stageTypeId)
+            .OrderByDescending(pse => pse.StartedAt)
+            .ToListAsync().ConfigureAwait(false);
+    }
+
+    public async Task<PipelineStepExecution> GetWithLogsAsync(int id)
+    {
+        return await this.DbSet
+            .Include(pse => pse.IssueLogs)
+            .FirstOrDefaultAsync(pse => pse.Id == id).ConfigureAwait(false);
+    }
+}
