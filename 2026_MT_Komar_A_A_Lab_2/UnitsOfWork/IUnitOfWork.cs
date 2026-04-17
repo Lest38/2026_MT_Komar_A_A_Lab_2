60399fb8a7531df1edf7eb95e5e92a53c7608923
Alina Komar <alinakomar11@gmail.com>
2026-04-17 17:43:35 +0300

    Add interface for unit of work

---
+++ 2026_MT_Komar_A_A_Lab_2/UnitsOfWork/IUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entities;
+using Repositories;
+
+namespace UnitsOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    IProjectRepository Projects { get; }
+
+    IRepository<StageType> StageTypes { get; }
+
+    IPipelineStepExecutionRepository PipelineStepExecutions { get; }
+
+    IIssueLogRepository IssueLogs { get; }
+
+    IRepository<CpuModel> CpuModels { get; }
+
+    IHostRepository Hosts { get; }
+
+    IPerformanceTestRepository PerformanceTests { get; }
+
+    IThreadSpeedMetricRepository ThreadSpeedMetrics { get; }
+
+    Task<int> SaveChangesAsync();
+
+    Task BeginTransactionAsync();
+
+    Task CommitTransactionAsync();
+
+    Task RollbackTransactionAsync();
+}
