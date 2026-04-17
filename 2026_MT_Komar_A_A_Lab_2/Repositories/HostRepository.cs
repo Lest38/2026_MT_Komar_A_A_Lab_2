534c3bae6e2c4b983d3aa26c6a48d94a4c37c6cd
Alina Komar <alinakomar11@gmail.com>
2026-04-17 19:37:46 +0300

    Add HostRepository and ProjectRepository

---
+++ 2026_MT_Komar_A_A_Lab_2/Repositories/HostRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace Repositories;
+
+using Data;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+#nullable enable
+public class HostRepository(ApplicationDbContext context)
+    : Repository<Host>(context), IHostRepository
+{
+    public async Task<Host?> GetDefaultHostAsync()
+    {
+        return await this.DbSet
+            .Include(h => h.CpuModel)
+            .FirstOrDefaultAsync().ConfigureAwait(false);
+    }
+
+    public async Task<Host?> GetWithCpuModelAsync(int id)
+    {
+        return await this.DbSet
+            .Include(h => h.CpuModel)
+            .FirstOrDefaultAsync(h => h.Id == id).ConfigureAwait(false);
+    }
+}
+#nullable restore
---
+++ 2026_MT_Komar_A_A_Lab_2/Repositories/ProjectRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Repositories;
+
+using Data;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+public class ProjectRepository(ApplicationDbContext context)
+    : Repository<Project>(context), IProjectRepository
+{
+    public async Task<Project> GetByFolderPathAsync(string folderPath)
+    {
+        return await this.DbSet.FirstOrDefaultAsync(p => p.FolderPath == folderPath).ConfigureAwait(false);
+    }
+
+    public async Task<Project> GetWithPipelineStepsAsync(int id)
+    {
+        return await this.DbSet
+            .Include(p => p.PipelineStepExecutions)
+            .ThenInclude(pse => pse.StageType)
+            .FirstOrDefaultAsync(p => p.Id == id).ConfigureAwait(false);
+    }
+}
