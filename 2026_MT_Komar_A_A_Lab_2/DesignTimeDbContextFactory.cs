d11a7622ae0eb440df8af4beac8e121fb8870ce0
Alina Komar <alinakomar11@gmail.com>
2026-04-23 11:56:35 +0300

    Add context factory

---
+++ 2026_MT_Komar_A_A_Lab_2/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DesignTimeDbContextFactory
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseSqlite("Data Source=app.db");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
