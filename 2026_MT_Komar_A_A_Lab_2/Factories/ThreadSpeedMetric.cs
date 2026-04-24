c5dc46c755e84b2899869c01a3f0ec970c6d01fd
Alina Komar <alinakomar11@gmail.com>
2026-04-24 18:19:27 +0300

    Add ThreadSpeedMetric

---
+++ 2026_MT_Komar_A_A_Lab_2/Factories/ThreadSpeedMetric.cs
@@ -0,0 +1,50 @@
+﻿namespace Factories;
+
+using Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable enable
+[Table("ThreadSpeedMetrics")]
+public class ThreadSpeedMetric
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    public int PerformanceTestId { get; set; }
+
+    [Required]
+    public int HostId { get; set; }
+
+    [Required]
+    public int PipelineStepExecutionId { get; set; }
+
+    [Required]
+    public long SequentialTimeMs { get; set; }
+
+    [Required]
+    public long ParallelTimeMs { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(10,4)")]
+    public decimal EfficiencyCoefficient { get; set; }
+
+    [Required]
+    public DateTime StartedAt { get; set; }
+
+    [Required]
+    public long DurationMs { get; set; }
+
+    [ForeignKey(nameof(PerformanceTestId))]
+    public virtual PerformanceTest? PerformanceTest { get; set; }
+
+    [ForeignKey(nameof(HostId))]
+    public virtual Host? Host { get; set; }
+
+    [ForeignKey(nameof(PipelineStepExecutionId))]
+    public virtual PipelineStepExecution? PipelineStepExecution { get; set; }
+}
+#nullable restore
