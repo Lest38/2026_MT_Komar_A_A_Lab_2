13c9ced9ed17e84017f6f3b3092829633a816a8a
Alina Komar <alinakomar11@gmail.com>
2026-04-24 11:05:03 +0300

    Add nullability to ProjectRepository methods

--- 2026_MT_Komar_A_A_Lab_2/Repositories/ProjectRepository.cs
+++ 2026_MT_Komar_A_A_Lab_2/Repositories/ProjectRepository.cs
@@ -5,15 +5,16 @@ using Entities;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
+#nullable enable
 public class ProjectRepository(ApplicationDbContext context)
     : Repository<Project>(context), IProjectRepository
 {
-    public async Task<Project> GetByFolderPathAsync(string folderPath)
+    public async Task<Project?> GetByFolderPathAsync(string folderPath)
     {
         return await this.DbSet.FirstOrDefaultAsync(p => p.FolderPath == folderPath).ConfigureAwait(false);
     }
 
-    public async Task<Project> GetWithPipelineStepsAsync(int id)
+    public async Task<Project?> GetWithPipelineStepsAsync(int id)
     {
         return await this.DbSet
             .Include(p => p.PipelineStepExecutions)
@@ -21,3 +22,4 @@ public class ProjectRepository(ApplicationDbContext context)
             .FirstOrDefaultAsync(p => p.Id == id).ConfigureAwait(false);
     }
 }
+#nullable disable
