d5d697409c9c08e38804500ce85686914e140d7c
Alina Komar <alinakomar11@gmail.com>
2026-04-24 11:21:41 +0300

    Add implemented IssueLogRepository

---
+++ 2026_MT_Komar_A_A_Lab_2/Repositories/IssueLogRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace Repositories;
+
+using Data;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class IssueLogRepository(ApplicationDbContext context)
+    : Repository<IssueLog>(context), IIssueLogRepository
+{
+    public async Task<IEnumerable<IssueLog>> GetByPipelineStepExecutionIdAsync(int pipelineStepExecutionId)
+    {
+        return await this.DbSet
+            .Where(il => il.PipelineStepExecutionId == pipelineStepExecutionId)
+            .OrderBy(il => il.LoggedAt)
+            .ToListAsync().ConfigureAwait(false);
+    }
+
+    public async Task<IEnumerable<IssueLog>> GetErrorsByPipelineStepExecutionIdAsync(int pipelineStepExecutionId)
+    {
+        return await this.DbSet
+            .Where(il => il.PipelineStepExecutionId == pipelineStepExecutionId && il.Severity == "Error")
+            .OrderBy(il => il.LoggedAt)
+            .ToListAsync().ConfigureAwait(false);
+    }
+
+    public async Task<IEnumerable<IssueLog>> GetWarningsByPipelineStepExecutionIdAsync(int pipelineStepExecutionId)
+    {
+        return await this.DbSet
+            .Where(il => il.PipelineStepExecutionId == pipelineStepExecutionId && il.Severity == "Warning")
+            .OrderBy(il => il.LoggedAt)
+            .ToListAsync().ConfigureAwait(false);
+    }
+}
