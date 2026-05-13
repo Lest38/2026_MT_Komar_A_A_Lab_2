86213030b0aca77b01e654aa9d79749fa47d4c6f
Alina Komar <alinakomar11@gmail.com>
2026-05-14 02:10:20 +0300

    Nullability and project fix

--- 2026_MT_Komar_A_A_Lab_2/Repositories/IPerformanceTestRepository.cs
+++ 2026_MT_Komar_A_A_Lab_2/Repositories/IPerformanceTestRepository.cs
@@ -3,7 +3,9 @@
 using Entities;
 using System.Threading.Tasks;
 
+#nullable enable
 public interface IPerformanceTestRepository : IRepository<PerformanceTest>
 {
-    Task<PerformanceTest> GetByDescriptionAsync(string description);
+    Task<PerformanceTest?> GetByDescriptionAsync(string description);
 }
+#nullable restore
